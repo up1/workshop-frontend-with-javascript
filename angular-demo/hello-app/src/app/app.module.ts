12fadec4b1a41d73b6a2b6f8b6b542bfb1894bed
up1 <somkiat.p@gmail.com>
2022-04-29 00:19:58 +0700

    Create component login

--- angular-demo/hello-app/src/app/app.module.ts
+++ angular-demo/hello-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
