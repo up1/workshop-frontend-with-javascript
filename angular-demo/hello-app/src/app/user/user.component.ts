4269a09944d7431002fb6ba174bb3a267b586ee7
up1 <somkiat.p@gmail.com>
2022-04-29 01:05:14 +0700

    Add html of list of user

--- angular-demo/hello-app/src/app/user/user.component.ts
+++ angular-demo/hello-app/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  users: User[] = [];
 
   ngOnInit(): void {
+    const fakeUsers = [
+      {id: 1, firstName: "User 1", lastName: "Last name 1", email: "user1@xxx.com"},
+      {id: 2, firstName: "User 2", lastName: "Last name 2", email: "user2@xxx.com"},
+    ];
+    this.users = fakeUsers;
   }
 
 }
